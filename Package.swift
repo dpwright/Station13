5fe52cc1014f91791faa0d52cfaf70770dc31e85
Daniel P. Wright <dani@dpwright.com>
2017-06-17 15:55:36 -0700

    Output the episode list as HTML via a Stencil template

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "site",
     dependencies: [
-        .Package(url: "https://github.com/nmdias/FeedKit.git", majorVersion: 6)
+        .Package(url: "https://github.com/nmdias/FeedKit.git", majorVersion: 6),
+        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,29 @@
 import Foundation
 import FeedKit
+import Stencil
+
+extension String {
+    func write(toFile path: String,
+               atomically useAuxiliaryFile: Bool,
+               encoding enc: String.Encoding,
+               creatingDirectory: Bool
+    ) throws {
+        if creatingDirectory {
+            let fileManager = FileManager.default
+            let parentPath = (path as NSString).deletingLastPathComponent
+            var isDirectory: ObjCBool = false
+            if !fileManager.fileExists(atPath: parentPath, isDirectory: &isDirectory) {
+                try fileManager.createDirectory(atPath: parentPath, withIntermediateDirectories: true, attributes: nil)
+            }
+            else if !isDirectory.boolValue {
+                print("Path \(parentPath) isn't a directory!")
+                abort()
+            }
+        }
+
+        try write(toFile: path, atomically: useAuxiliaryFile, encoding: enc)
+    }
+}
 
 let feedURL = URL(string: "http://station13.libsyn.com/rss")!
 guard let result = FeedParser(URL: feedURL)?.parse() else {
@@ -9,17 +33,21 @@ guard let result = FeedParser(URL: feedURL)?.parse() else {
 
 guard let feed = result.rssFeed,
       result.isSuccess else {
-    print(result.error)
+    print(result.error as Any)
     abort()
 }
 
-let podcastTitle = feed.title ?? "Unnamed Podcast"
-let podcastDescription = feed.description ?? ""
+let fsLoader = FileSystemLoader(paths: ["Templates/"])
+let environment = Environment(loader: fsLoader)
 
-print(podcastTitle)
-print(podcastDescription)
+let context : [String: Any] = [
+    "title" : feed.title ?? "Unnamed Podcast",
+    "items" : feed.items ?? []
+]
 
-for item in feed.items ?? [] {
-    print(item.title ?? "")
-    print(item.pubDate ?? "")
-}
+let template = try environment.loadTemplate(name: "index.html")
+let index = try template.render(context)
+try index.write(toFile            : "Site/index.html",
+                atomically        : true,
+                encoding          : .utf8,
+                creatingDirectory : true)
