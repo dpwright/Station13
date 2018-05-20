3c42076f5663ff2a88fb64f683ba4d2060cf77b8
Daniel P. Wright <dani@dpwright.com>
2018-05-19 17:23:43 -0700

    Move site generation code into subdirectory

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "site",
+    name: "station13",
     dependencies: [
         .Package(url: "https://github.com/nmdias/FeedKit.git", majorVersion: 6),
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
